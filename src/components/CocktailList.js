23ead8ed7b27e9db82fc10ab94bb945f22d95dc6
Driijko <driijko@gmail.com>
2020-03-10 15:49:15 -0400

    Front-end can receive and display an indefinite number of cocktails from back-end

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 // Import Components
 import {Cocktail} from './styled components/Cocktail';
+import {Title} from '../components/styled components/Title';
 
 // Import helper functions
 import randomColor from '../helper functions/randomColor';
@@ -13,27 +14,36 @@ export default function CocktailList(props) {
   // STATE
   const [dataObtained, setDataObtained] = useState(false);
   const [cocktailsData, setCocktailsData] = useState([]);
+  const [cocktailComponents, setCocktailComponents] = useState(null);
 
   useEffect(()=> {
     axios.get('http://localhost:8000/cocktailList')
       .then( res => {
-          setCocktailsData(res.data)
+          setCocktailsData(res.data);
+          const cocktailComponentsArray = [];
+          for (let i = 0 ; i < res.data.length ; i++) {
+            cocktailComponentsArray.push(<Cocktail key={i} color={randomColor()}>{res.data[i].name}</Cocktail>);
+          }
+          // console.log(cocktailComponentsArray);
+          setCocktailComponents(cocktailComponentsArray);
           setDataObtained(true);
-          // console.log(res.data);
       })
   }, [])
 
   // useEffect(()=> {
-  //   console.log(cocktailsData);
-  // }, [cocktailsData]);
+  //   console.log(cocktailComponents);
+  //   // console.log(cocktailsData);
+  // }, [cocktailComponents]);
 
   return (
     <div>
+      <Title>COCKTAILS</Title>
       { 
         dataObtained ?
         <div> 
-          <Cocktail color={randomColor()}>{cocktailsData[0].name}</ Cocktail>
-          <Cocktail color={randomColor()}>{cocktailsData[1].name}</Cocktail>
+          {/* <Cocktail color={randomColor()}>{cocktailsData[0].name}</ Cocktail>
+          <Cocktail color={randomColor()}>{cocktailsData[1].name}</Cocktail> */}
+          {cocktailComponents}
         </div>
         : null
       }
